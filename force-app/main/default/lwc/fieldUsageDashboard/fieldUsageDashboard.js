7a9d69ce29408759ddfce6f252cdf86eb028574f
SatyamMishra2001 <mishrasatyam626@gmail.com>
2026-03-09 17:21:33 +0530

    Enhanced Calculation

--- force-app/main/default/lwc/fieldUsageDashboard/fieldUsageDashboard.js
+++ force-app/main/default/lwc/fieldUsageDashboard/fieldUsageDashboard.js
@@ -1,77 +1,15 @@
-import { LightningElement, wire, track } from 'lwc';
-import getFieldUsage from '@salesforce/apex/FieldUsageDashboardController.getFieldUsage';
+import { LightningElement, api, wire } from 'lwc';
+import getObjectHealth from '@salesforce/apex/FieldHealthController.getObjectHealth';
 
-export default class FieldUsageDashboard extends LightningElement {
+export default class FieldHealthPanel extends LightningElement {
+   @api objectApiName = 'Account'; // Defaults to Account, but dynamically updates based on the page
+    healthData;
+    error;
 
-    @track fullData = [];
-    @track paginatedData = [];
-
-    pageSize = 10;
-    currentPage = 1;
-    totalPages = 1;
-
-    @wire(getFieldUsage)
-    wiredUsage({ error, data }) {
-
-        if (data) {
-
-            // Map risk class
-            this.fullData = data.map(item => {
-                return {
-                    ...item,
-                    riskClass: item.riskColor
-                };
-            });
-
-            this.totalPages = Math.ceil(this.fullData.length / this.pageSize);
-
-            this.currentPage = 1;
-
-            this.updatePagination();
-        }
-
-        else if (error) {
-            console.error(error);
-        }
+    // This dynamically creates the title!
+    get dynamicTitle() {
+        return `${this.objectApiName} Field Health Panel`;
     }
-
-    updatePagination() {
-
-        const start = (this.currentPage - 1) * this.pageSize;
-        const end = start + this.pageSize;
-
-        this.paginatedData = this.fullData
-            .slice(start, end)
-            .map((item, index) => {
-
-                return {
-                    ...item,
-                    rowNumber: start + index + 1
-                };
-            });
-    }
-
-    handleNext() {
-
-        if (this.currentPage < this.totalPages) {
-            this.currentPage++;
-            this.updatePagination();
-        }
-    }
-
-    handlePrevious() {
-
-        if (this.currentPage > 1) {
-            this.currentPage--;
-            this.updatePagination();
-        }
-    }
-
-    get disablePrevious() {
-        return this.currentPage <= 1;
-    }
-
-    get disableNext() {
-        return this.currentPage >= this.totalPages;
-    }
+    @wire(getObjectHealth, { objectName: '$objectApiName' })
+    metrics;
 }

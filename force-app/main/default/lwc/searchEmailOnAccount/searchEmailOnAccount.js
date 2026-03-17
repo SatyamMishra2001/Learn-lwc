912fc964f1b893a0c5633841f3c11b1ab15c4325
SatyamMishra2001 <mishrasatyam626@gmail.com>
2026-03-17 22:28:34 +0530

    Search Account based on Email

---
+++ force-app/main/default/lwc/searchEmailOnAccount/searchEmailOnAccount.js
@@ -0,0 +1,27 @@
+import { LightningElement } from 'lwc';
+import searchAccountByEmail from '@salesforce/apex/fetchAccounts.getRecords';
+
+export default class SearchEmailOnAccount extends LightningElement {
+
+    email;
+    account;
+
+
+    handleEmailChange(event){
+        this.email = event.target.value;
+
+    }
+
+    handleSearch(){
+        searchAccountByEmail({Email : this.email})
+        .then(result => {
+            this.account = result;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+     }
+    
+
+
+}
